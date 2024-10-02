ac2674ff99a6599417849279605e6cd356ca19ae
Nurbol0927 <program.nr0927@gmail.com>
2024-10-02 13:37:07 +0600

    comid

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+String[] nur ={"Umar","Nurbol","Sanjar"};
+
+
+        for (String name : nur) {
+            System.out.println(name + " is Java develoer");
+      //  public static int num ( int ...number){
+
+        }
+
+    }
+}
